66cce83ae764d2893042f808fdccc3948ec94f75
Rahul Siloniya <38026849+rahulsiloniya@users.noreply.github.com>
2021-12-04 08:52:13 +0530

    Update binarySearch.cpp

--- binarySearch.cpp
+++ binarySearch.cpp
@@ -1,31 +1,50 @@
-#include <vector>
 #include <iostream>
 
 using namespace std;
 
+int binar(int arr[], int high, int low, int val, int &step);
 
-//int main() {
-//    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
-//    int k;
-//    int ki;
-//    int n;      //size of array of vector
-//    int q;      //number of queries
-//    int i;
-//    int j;
-//    cin >> n >> q;
-//    vector<int>* arr = new vector<int>[n];
-//    //vector<int> arr[n];
-//    
-//    for (int i = 0; i < n; i++) {
-//        cin >> k;
-//        for (int j = 0; j < k; j++) {
-//            cin >> ki;
-//            arr[i].push_back(ki);
-//        }
-//    }
-//    for (int i = 0; i < q; i++) {
-//        cin >> i >> j;
-//        cout << arr[i][j] << "\n";
-//    }
-//    return 0;
+int main() {
+	int size;
+	int val = 11;
+	int steps = 0;
+	int srtArr[10] = { 1, 3, 5, 7, 9, 11, 13, 15, 17, 19 };
+	size = 10;
+	/*for (int i = 0; i < size; i++) {
+		cout << binar(srtArr, size - 1, 0, srtArr[i]) << "\n";
+	}*/
+	cout << binar(srtArr, size - 1, 0, 11, steps) << "\n";              //modify the fourth argument to search a different element
+	cout << "Number of steps done to search 11 in given array are " << steps << "\n";
+}
+
+//int sortedSearch(int arr[], int size, int val) {
+//	int start = 0;
+//	int end = size - 1;
+//	int mid = (start + end) / 2;
+//	while (start <= end) {
+//		if (arr[mid] == val) return mid;
+//		else if (arr[mid] < val) {
+//			start = mid + 1;
+//			mid = (start + end) / 2;
+//		}
+//		else {
+//			end = mid - 1;
+//			mid = (start + end) / 2;
+//		}
+//	}
+//	return -1;
 //}
+int binar(int arr[], int high, int low, int val, int &step) {
+	int mid = (high + low) / 2;
+	step++;
+	if (arr[mid] == val) return mid;
+	else if (low <= high) {
+		if (arr[mid] < val) {
+			return binar(arr, high, mid + 1, val, step);
+		}
+		else {
+			return binar(arr, mid - 1, low, val, step);
+		}
+	}
+	else { return -1; }
+}
