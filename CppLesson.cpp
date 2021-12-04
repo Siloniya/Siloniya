75a181b8662ba7bd62be26713c33bf429d5c9e16
Rahul Siloniya <38026849+rahulsiloniya@users.noreply.github.com>
2021-12-04 08:48:23 +0530

    Binary search using loop and recursion
    Contains the implementation of binary search using loop (contained in comments) and recursion. The program also returns the number of steps completed to reach the required result. A pass-by-reference parameter is used to update a variable in the recursive implementation to increment the step count.

---
+++ CppLesson.cpp
@@ -0,0 +1,31 @@
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+
+//int main() {
+//    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+//    int k;
+//    int ki;
+//    int n;      //size of array of vector
+//    int q;      //number of queries
+//    int i;
+//    int j;
+//    cin >> n >> q;
+//    vector<int>* arr = new vector<int>[n];
+//    //vector<int> arr[n];
+//    
+//    for (int i = 0; i < n; i++) {
+//        cin >> k;
+//        for (int j = 0; j < k; j++) {
+//            cin >> ki;
+//            arr[i].push_back(ki);
+//        }
+//    }
+//    for (int i = 0; i < q; i++) {
+//        cin >> i >> j;
+//        cout << arr[i][j] << "\n";
+//    }
+//    return 0;
+//}
